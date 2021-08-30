491a5eead7680acf4e43b5f1a7df6b8e221fe0f3
Jan-André Zinser <zinserjan@gmail.com>
2021-08-30 14:09:48 +0200

    feat(app-state): provide state management for react powered by mobx (#2)

---
+++ packages/app-state/src/index.ts
@@ -0,0 +1,29 @@
+// config
+import { IPromiseBasedObservable } from "mobx-utils";
+
+export { configure } from "mobx";
+
+// Creating observables
+export { makeAutoObservable, makeObservable, observable, computed } from "mobx";
+
+// Actions
+export { action, runInAction, flow, flowResult } from "mobx";
+
+// Collection utilities
+export { values, keys, entries, get, has, set, remove } from "mobx";
+
+// Reactions
+export { autorun, reaction, when } from "mobx";
+
+// react
+export { observer, Observer, disposeOnUnmount } from "mobx-react";
+
+// utilies
+export { toJS } from "mobx";
+
+// debug, see https://mobx.js.org/analyzing-reactivity.html
+export { trace } from "mobx";
+
+// utils
+export { fromPromise, computedFn } from "mobx-utils";
+export type ObservablePromise<T> = IPromiseBasedObservable<T>;
---
+++ packages/app-state/test/integration/decorator.test.ts
@@ -0,0 +1,100 @@
+import {
+  computed,
+  makeObservable,
+  observable,
+  runInAction,
+  toJS,
+  when,
+} from "../../src";
+
+describe("decorator", () => {
+  it("simple property", async () => {
+    class Store {
+      @observable
+      stringProperty: string = "";
+
+      @observable.shallow
+      arrayProperty: Array<{ id: string }> = [];
+
+      @observable.struct
+      objectProperty: { id: string } = { id: "" };
+
+      constructor() {
+        makeObservable(this);
+      }
+
+      @computed
+      get objectPropertyAsString(): string {
+        return JSON.stringify(toJS(this.objectProperty));
+      }
+    }
+
+    const store = new Store();
+
+    expect(store.stringProperty).toBe("");
+    expect(store.arrayProperty).toEqual([]);
+    expect(store.objectProperty).toEqual({ id: "" });
+
+    const promise = when(() => store.stringProperty != "", { timeout: 500 });
+
+    runInAction(() => {
+      store.stringProperty = "hello world";
+    });
+
+    await promise;
+
+    expect(store.stringProperty).toBe("hello world");
+  });
+
+  it("array property", async () => {
+    class Store {
+      @observable.shallow
+      arrayProperty: Array<{ id: number }> = [];
+
+      constructor() {
+        makeObservable(this);
+      }
+    }
+
+    const store = new Store();
+
+    expect(store.arrayProperty).toEqual([]);
+
+    const promise = when(() => store.arrayProperty.length > 0, {
+      timeout: 500,
+    });
+
+    runInAction(() => {
+      store.arrayProperty.push({ id: 1 });
+    });
+
+    await promise;
+
+    expect(store.arrayProperty).toEqual([{ id: 1 }]);
+  });
+
+  it("objectProperty property", async () => {
+    class Store {
+      @observable
+      objectProperty: { id: number } = { id: 0 };
+
+      constructor() {
+        makeObservable(this);
+      }
+    }
+
+    const store = new Store();
+
+    expect(store.objectProperty).toEqual({ id: 0 });
+
+    const promise = when(() => store.objectProperty.id !== 0, { timeout: 500 });
+
+    runInAction(() => {
+      store.objectProperty.id = 1;
+    });
+
+    await promise;
+
+    expect(store.objectProperty).toEqual({ id: 1 });
+  });
+});
