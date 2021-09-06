828a9448a8954d190e0fe7f570da00a1486f2729
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2021-09-06 17:34:19 +0200

    chore(storybook): setup storybook for testing

---
+++ packages/storybook/stories/folder/I18n.stories.tsx
@@ -0,0 +1,87 @@
+import {
+  Container,
+  ContainerProvider,
+  I18nService,
+  createI18nModuleRegistry,
+  fromPromise,
+  observer,
+  useI18n,
+  useInjection,
+} from "@cpro-js/react-core";
+import { Meta } from "@storybook/react";
+import { FC, useEffect, useMemo } from "react";
+
+const createContainer = async () => {
+  const container = new Container();
+
+  let counter = 0;
+
+  // i18n
+  await container.loadAsync(
+    createI18nModuleRegistry({
+      debug: true,
+      determineLocale: () => "de-DE",
+      fallbackLocale: "de-DE",
+      fallbackTimezone: "Europe/Berlin",
+      supportedLocales: ["de-DE"],
+      getTranslations: (language) => {
+        console.log("getTranslations", language);
+        return Promise.resolve({
+          hello: {
+            world: `Hello World ${language} - ${++counter}`,
+          },
+        });
+      },
+    })
+  );
+
+  return container;
+};
+
+const Provider: FC<{}> = observer(({ children }) => {
+  const container = useMemo(() => {
+    return fromPromise(createContainer());
+  }, []);
+
+  return container.case({
+    pending: () => <div>Loading...</div>,
+    fulfilled: (container: Container) => (
+      <ContainerProvider container={container}>{children}</ContainerProvider>
+    ),
+    rejected: (error: Error) => (
+      <div>
+        Error {error.message} {error.stack}
+      </div>
+    ),
+  });
+});
+
+const Consumer = observer(() => {
+  const { t } = useI18n();
+
+  const i18nService = useInjection(I18nService);
+
+  useEffect(() => {
+    const reload = setTimeout(() => {
+      i18nService.useLocale(i18nService.getLocale());
+    }, 2000);
+
+    return () => {
+      clearTimeout(reload);
+    };
+  }, [i18nService]);
+
+  return <div>{t("hello.world")}</div>;
+});
+
+export default {
+  title: "I18n",
+} as Meta;
+
+export const Default = () => {
+  return (
+    <Provider>
+      <Consumer />
+    </Provider>
+  );
+};
