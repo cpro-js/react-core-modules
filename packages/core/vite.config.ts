fcaa9fa02a0dfc2d35d94aa4a040bac3256a9bc9
Jan-André Zinser <zinserjan@gmail.com>
2025-02-04 09:36:30 +0100

    feat: use vite as build system and vitest for testing

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,19 +4,42 @@ import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { defineConfig } from "vitest/config";
 
+import packageJson from "./package.json";
+
 export default defineConfig({
   build: {
     emptyOutDir: true,
     sourcemap: true,
-    ssr: true, // disable bundling of node_modules
+    minify: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      output: {
+        interop: "auto", // transforms default import into require('xxx').default;
+      },
+      external: [
+        ...Object.keys(packageJson.dependencies),
+        ...Object.keys(packageJson.peerDependencies),
+      ],
     },
   },
   plugins: [
     react({
       babel: {
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              modules: false, // false will preserve ES modules
+              targets: "last 2 versions, > 0.2%, not dead",
+              loose: true,
+            },
+          ],
+          "@babel/preset-typescript",
+        ],
         plugins: [
           "babel-plugin-transform-typescript-metadata",
           ["@babel/plugin-proposal-decorators", { legacy: true }],
@@ -24,7 +47,6 @@ export default defineConfig({
         ],
       },
     }),
-    react(),
     dts({
       rollupTypes: true,
       include: ["src"],
