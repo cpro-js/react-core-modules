9d8277d3c630d117f64da07e7238aaeeca58e33a
Jan-André Zinser <zinserjan@gmail.com>
2025-02-04 22:23:29 +0100

    chore: pure ESM packages

--- packages/i18n/vite.config.ts
+++ packages/i18n/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     minify: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs"],
+      formats: ["es"],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
